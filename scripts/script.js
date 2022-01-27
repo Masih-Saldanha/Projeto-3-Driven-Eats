1fd3d8dbdd420c2e00608d64cc626f6a9db4f062
Masih Saldanha Xavier <masih.saldanha@gmail.com>
2022-01-27 13:31:44 -0300

    Iniciado onclick com Java Script

---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+function selecionar() {
+    // document.getElementsByClassName("menu-opcao").style.boxShadow = "0px 4px 4px rgba(0, 0, 0, 0.25), inset 0px 0px 0px 5px #32B72F";
+    document.getElementById("pizza").style.boxShadow = "0px 4px 4px rgba(0, 0, 0, 0.25), inset 0px 0px 0px 5px #32B72F";
+    document.getElementById("pizza-sinal-verde").style.display = "flex";
+}
