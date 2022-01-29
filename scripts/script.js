e7a2a895ac3958525ed77dce0ed5e8164e4fbf96
Masih Saldanha Xavier <masih.saldanha@gmail.com>
2022-01-29 15:55:58 -0300

    Implementada mensagem automática do Zap Zap

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,44 @@
 // VARIÁVEIS PARA MANIPULAÇÃO
+// VARIÁVEIS DE SELEÇÃO DE PRATO
 let escolherPizzaSinal = document.querySelector(".pizza-sinal-verde");
 let escolherPizzaBorda = document.querySelector(".menu-opcao-pizza");
 let escolherLasanhaSinal = document.querySelector(".lasanha-sinal-verde");
 let escolherLasanhaBorda = document.querySelector(".menu-opcao-lasanha");
 let escolherSushiSinal = document.querySelector(".sushi-sinal-verde");
 let escolherSushiBorda = document.querySelector(".menu-opcao-sushi");
+// VARIÁVEIS DE SELEÇÃO DE BEBIDA
 let escolherRefrigeranteSinal = document.querySelector(".refrigerante-sinal-verde");
 let escolherRefrigeranteBorda = document.querySelector(".menu-opcao-refrigerante");
 let escolherSucoSinal = document.querySelector(".suco-sinal-verde");
 let escolherSucoBorda = document.querySelector(".menu-opcao-suco");
 let escolherCocoSinal = document.querySelector(".coco-sinal-verde");
 let escolherCocoBorda = document.querySelector(".menu-opcao-coco");
+// VARIÁVEIS DE SELEÇÃO DE SOBREMESA
 let escolherBoloSinal = document.querySelector(".bolo-sinal-verde");
 let escolherBoloBorda = document.querySelector(".menu-opcao-bolo");
 let escolherSorveteSinal = document.querySelector(".sorvete-sinal-verde");
 let escolherSorveteBorda = document.querySelector(".menu-opcao-sorvete");
 let escolherPudimSinal = document.querySelector(".pudim-sinal-verde");
 let escolherPudimBorda = document.querySelector(".menu-opcao-pudim");
+// VARIÁVEIS DE PRECO
+let precoPrato = 0;
+let precoBebida = 0;
+let precoSobremesa = 0;
+let precoTotal = precoPrato+precoBebida+precoSobremesa;
 
 let botaoVerde = document.querySelector(".barra-baixo-botao");
-
+// VARIÁVEIS DE CHECAGEM PARA HABILITAÇÃO DO BOTÃO VERDE
 let pedidoFeitoPrato = 0;
 let pedidoFeitoBebida = 0;
 let pedidoFeitoSobremesa = 0;
 
 let pedidoFeito = pedidoFeitoPrato+pedidoFeitoBebida+pedidoFeitoSobremesa;
+// VARIÁVEIS DE NOMES DOS PEDIDOS
+let nomePrato = "";
+let nomeBebida = "";
+let nomeSobremesa = "";
 
+let mensagemZap = "";
 // FUNÇÃO QUE VERIFICA E LIBERA O BOTÃO VERDE DE BAIXO
 function habilitarPedido() {
     pedidoFeito = pedidoFeitoPrato+pedidoFeitoBebida+pedidoFeitoSobremesa;
@@ -44,6 +57,8 @@ function selecionarPizza() {
     escolherLasanhaBorda.classList.add("menu-opcao-lasanha-escondido");
     escolherSushiSinal.classList.add("sushi-sinal-verde-escondido");
     escolherSushiBorda.classList.add("menu-opcao-sushi-escondido");
+    precoPrato = 20;
+    nomePrato = "Pizza de Capituri";
     pedidoFeitoPrato = 1;
     habilitarPedido();
 }
@@ -54,6 +69,8 @@ function selecionarLasanha() {
     escolherLasanhaBorda.classList.remove("menu-opcao-lasanha-escondido");
     escolherSushiSinal.classList.add("sushi-sinal-verde-escondido");
     escolherSushiBorda.classList.add("menu-opcao-sushi-escondido");
+    precoPrato = 15;
+    nomePrato = "Lasanha de Frango";
     pedidoFeitoPrato = 1;
     habilitarPedido();
 }
@@ -64,6 +81,8 @@ function selecionarSushi() {
     escolherLasanhaBorda.classList.add("menu-opcao-lasanha-escondido");
     escolherSushiSinal.classList.remove("sushi-sinal-verde-escondido");
     escolherSushiBorda.classList.remove("menu-opcao-sushi-escondido");
+    precoPrato = 25;
+    nomePrato = "Barca de Sushi";
     pedidoFeitoPrato = 1;
     habilitarPedido();
 }
@@ -76,6 +95,8 @@ function selecionarRefrigerante() {
     escolherSucoBorda.classList.add("menu-opcao-suco-escondido");
     escolherCocoSinal.classList.add("coco-sinal-verde-escondido");
     escolherCocoBorda.classList.add("menu-opcao-coco-escondido");
+    precoBebida = 6;
+    nomeBebida = "Lata de Refrigerante";
     pedidoFeitoBebida = 1;
     habilitarPedido();
 }
@@ -86,6 +107,8 @@ function selecionarSuco() {
     escolherSucoBorda.classList.remove("menu-opcao-suco-escondido");
     escolherCocoSinal.classList.add("coco-sinal-verde-escondido");
     escolherCocoBorda.classList.add("menu-opcao-coco-escondido");
+    precoBebida = 7;
+    nomeBebida = "Suquinho de Laranja";
     pedidoFeitoBebida = 1;
     habilitarPedido();
 }
@@ -96,6 +119,8 @@ function selecionarCoco() {
     escolherSucoBorda.classList.add("menu-opcao-suco-escondido");
     escolherCocoSinal.classList.remove("coco-sinal-verde-escondido");
     escolherCocoBorda.classList.remove("menu-opcao-coco-escondido");
+    precoBebida = 8;
+    nomeBebida = "Água de Coco";
     pedidoFeitoBebida = 1;
     habilitarPedido();
 }
@@ -108,6 +133,8 @@ function selecionarBolo() {
     escolherSorveteBorda.classList.add("menu-opcao-sorvete-escondido");
     escolherPudimSinal.classList.add("pudim-sinal-verde-escondido");
     escolherPudimBorda.classList.add("menu-opcao-pudim-escondido");
+    precoSobremesa = 11;
+    nomeSobremesa = "Bolo de Cenoura";
     pedidoFeitoSobremesa = 1;
     habilitarPedido();
 }
@@ -118,6 +145,8 @@ function selecionarSorvete() {
     escolherSorveteBorda.classList.remove("menu-opcao-sorvete-escondido");
     escolherPudimSinal.classList.add("pudim-sinal-verde-escondido");
     escolherPudimBorda.classList.add("menu-opcao-pudim-escondido");
+    precoSobremesa = 12;
+    nomeSobremesa = "Sorvete";
     pedidoFeitoSobremesa = 1;
     habilitarPedido();
 }
@@ -128,12 +157,17 @@ function selecionarPudim() {
     escolherSorveteBorda.classList.add("menu-opcao-sorvete-escondido");
     escolherPudimSinal.classList.remove("pudim-sinal-verde-escondido");
     escolherPudimBorda.classList.remove("menu-opcao-pudim-escondido");
+    precoSobremesa = 13;
+    nomeSobremesa = "Pudim";
     pedidoFeitoSobremesa = 1;
     habilitarPedido();
 }
 
 // APERTAR BOTÃO VERDE
 function fecharPedido() {
+    precoTotal = precoPrato + precoBebida + precoSobremesa;
+    mensagemZap = `Olá, gostaria de fazer o pedido:\n-Prato: ${nomePrato}\n-Bebida: ${nomeBebida}\n-Sobremesa: ${nomeSobremesa}\nTotal: R$ ${precoTotal.toFixed(2)}`;
     if (pedidoFeito === 3) {
+        window.open(`https://wa.me/5593991970827?text=${encodeURIComponent(mensagemZap)}`);
     }
 }
